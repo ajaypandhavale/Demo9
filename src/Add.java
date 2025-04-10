b7f3be17fa6a25110136eab09089c0ce200f53f6
ajaypandhavale <aj40135821@gmail.com>
2025-04-10 15:55:47 +0530

    added line

---
+++ src/Add.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum=0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number to perform addtion");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        sum = a + b;
+        System.out.println("addtion is :"+sum);
+
+        System.out.println("Added line");
+
+    }
+}
